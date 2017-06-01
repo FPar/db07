9279ea9be73d83596057cd079518c2456f036a1d
Fabian Parzefall <fabian.parzefall@mailbox.org>
2017-06-01 11:41:33 +0200

    Fix build

---
+++ src/core/plan/Table_scan.cpp
@@ -0,0 +1,14 @@
+#include "Table_scan.h"
+
+using namespace db07;
+
+bool Table_scan::has_next()
+{
+    return false;
+}
+
+Row Table_scan::next()
+{
+    std::vector<Value*> values;
+    return Row(nullptr, values);
+}
