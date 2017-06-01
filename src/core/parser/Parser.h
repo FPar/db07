381cc320619c1acfec3f9087e57785ceb13b44a7
Marcel Binder <marcel.binder@mailbox.org>
2017-06-01 11:56:43 +0200

    first implementation of parser, tests

---
+++ src/core/parser/Parser.h
@@ -0,0 +1,34 @@
+#ifndef DB07_PARSER_H
+#define DB07_PARSER_H
+
+#include <list>
+#include "Token.h"
+
+namespace db07 {
+    class Parser {
+    private:
+        std::list<Token>::const_iterator lookahead;
+        std::list<Token>::const_iterator end;
+    public:
+        void parse(std::list<Token> &tokens);
+
+    private:
+        bool terminal(token_type);
+
+        bool start();
+
+        bool identifier();
+
+        bool optional_where();
+
+        bool lookahead_is(token_type type);
+
+        bool end_of_tokens();
+
+        bool where_conditions();
+
+        bool where_condition();
+    };
+}
+
+#endif
---
+++ tests/parser/Parser_test.cpp
@@ -0,0 +1,15 @@
+#include "catch.h"
+#include "parser/Parser.h"
+
+namespace db07 {
+    TEST_CASE("parser_select_from", "[Parser]") {
+        std::list<Token> tokens;
+        tokens.push_back(Token(token_type::KEYWORD_SELECT));
+        tokens.push_back(Token(token_type::KEYWORD_FROM));
+        tokens.push_back(Token(token_type::IDENTIFIER));
+        tokens.push_back(Token(token_type::KEYWORD_WHERE));
+        tokens.push_back(Token(token_type::IDENTIFIER));
+        Parser parser;
+        parser.parse(tokens);
+    };
+}
