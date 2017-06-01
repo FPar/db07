0156f68b2646492237cc08351657d08789aa000f
Marcel Binder <marcel.binder@mailbox.org>
2017-06-01 17:35:27 +0200

    please don't break the build too long

--- src/core/storage_engine/Btree.cpp
+++ src/core/storage_engine/Btree.cpp
@@ -1,39 +1,39 @@
-#include "Btree.h"
-
-void db07::Btree::insert(int index, Row entries, Node *node) {
-    //Check if node has been initialized
-    if(node == nullptr){
-        node = new Node();
-        insert(index, entries, node);
-    }else {
-        //Check if new entry can be inserted in current Node
-        if(node->setKeys == AMOUNTKEYS){
-            insertFullNode(index, entries, node);
-        }else {
-            insertSpaceNode(index, entries, node);
-        }
-    }
-}
-
-void db07::Btree::insertFullNode(int index, Row entries, Node *node) {
-    int counter = 0;
-    for(auto i = node->keys.cbegin(); i < node->keys.cend(); i++){
-        if(index < (*i)){
-            insert(index, entries, &(node->childNodes[counter]));
-        } else {
-            if(i == node->keys.end()){
-                insert(index, entries, &(node->childNodes[counter + 1]));
-            }
-        }
-        counter++;
-    }
-}
 
 
-void db07::Btree::insertSpaceNode(int index, Row entries, Node *node) {
-    for(auto i = node->keys.cbegin(); i < node->keys.cend(); i++){
-
-    }
-}
+//void db07::Btree::insert(int index, Row entries, Node *node) {
+//    //Check if node has been initialized
+//    if(node == nullptr){
+//        node = new Node();
+//        insert(index, entries, node);
+//    }else {
+//        //Check if new entry can be inserted in current Node
+//        if(node->setKeys == AMOUNTKEYS){
+//            insertFullNode(index, entries, node);
+//        }else {
+//            insertSpaceNode(index, entries, node);
+//        }
+//    }
+//}
+//
+//void db07::Btree::insertFullNode(int index, Row entries, Node *node) {
+//    int counter = 0;
+//    for(auto i = node->keys.cbegin(); i < node->keys.cend(); i++){
+//        if(index < (*i)){
+//            insert(index, entries, &(node->childNodes[counter]));
+//        } else {
+//            if(i == node->keys.end()){
+//                insert(index, entries, &(node->childNodes[counter + 1]));
+//            }
+//        }
+//        counter++;
+//    }
+//}
+//
+//
+//void db07::Btree::insertSpaceNode(int index, Row entries, Node *node) {
+//    for(auto i = node->keys.cbegin(); i < node->keys.cend(); i++){
+//
+//    }
+//}
 
 
--- src/core/storage_engine/Btree.h
+++ src/core/storage_engine/Btree.h
@@ -14,33 +14,33 @@ namespace db07 {
 
     class Btree {
 
-    private:
-        struct Node {
-            std::array<int, AMOUNTKEYS> keys = {-1};
-            std::array<Node, AMOUNTKEYS+1> childNodes = {nullptr};
-            int setKeys = 0;
-        };
-
-
-        struct LeafNode : Node {
-            int index = -1;
-            Row entries;
-        };
-
-        Node *root;
-
-        void insertFullNode(int index, Row entries, Node *node);
-
-        void insertSpaceNode(int index, Row entries, Node *node);
-
-    public:
-        void insert(int index, Row entries, Node *node);
-
-        void remove(int index);
-
-        void indexSeek(int index);
-
-        void indexScan(Row *values);
+//    private:
+//        struct Node {
+//            std::array<int, AMOUNTKEYS> keys = {-1};
+//            std::array<Node, AMOUNTKEYS+1> childNodes = {nullptr};
+//            int setKeys = 0;
+//        };
+//
+//
+//        struct LeafNode : Node {
+//            int index = -1;
+//            Row entries;
+//        };
+//
+//        Node *root;
+//
+//        void insertFullNode(int index, Row entries, Node *node);
+//
+//        void insertSpaceNode(int index, Row entries, Node *node);
+//
+//    public:
+//        void insert(int index, Row entries, Node *node);
+//
+//        void remove(int index);
+//
+//        void indexSeek(int index);
+//
+//        void indexScan(Row *values);
 
 
     };
