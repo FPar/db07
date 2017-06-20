29247b442c8f3de203a2b996ac4612c88406dc27
Fabian Parzefall <fabian.parzefall@mailbox.org>
2017-06-20 22:52:54 +0200

    Define Hash_match signature

--- src/core/plan/Hash_match.cpp
+++ src/core/plan/Hash_match.cpp
@@ -1,1 +1,14 @@
 #include "Hash_match.h"
+
+using namespace std;
+using namespace db07;
+
+bool Hash_match::fetch_next()
+{
+    return false;
+}
+
+shared_ptr<Row> Hash_match::next()
+{
+    return std::shared_ptr<Row>();
+}
--- src/core/plan/Hash_match.h
+++ src/core/plan/Hash_match.h
@@ -5,17 +5,27 @@
 #include <map>
 #include <vector>
 #include "Plan_node.h"
-#include <storage_engine/Row.h>
+#include <storage_engine/Table.h>
 
 namespace db07 {
     class Hash_match : public Plan_node {
     public:
+        Hash_match(const std::shared_ptr<Table> &a, const std::shared_ptr<Table> &b, int col_key_a, int col_key_b) :
+            a(a), b(b), col_key_a(col_key_a), col_key_b(col_key_b)
+        {
+        }
+
         bool fetch_next() override;
 
         std::shared_ptr<Row> next() override;
 
     private:
-        std::map<int, std::vector<std::shared_ptr<Row>>> _map;
+        std::shared_ptr<Table> a;
+        std::shared_ptr<Table> b;
+        int col_key_a;
+        int col_key_b;
+
+        std::map<int, std::vector<std::shared_ptr<Row>>> matching;
     };
 }
 
