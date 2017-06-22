e79e1a688a959d8d3312fd89d513099f9f5be5da
Sebastian Horvath <se.bi1994@hotmail.de>
2017-06-22 12:05:17 +0200

    changed query data to reflect the needs of the parser

--- src/core/optimizer/Query_data.cpp
+++ src/core/optimizer/Query_data.cpp
@@ -5,28 +5,32 @@
 
 namespace db07 {
 
-    const Query_type Query_data::getQuery_type() const {
+     Query_type Query_data::getQuery_type() const {
         return _queryType;
     }
 
-    std::string Query_data::getTableName() {
+    std::vector<std::string> &Query_data::getTableName() {
         return _tableName;
     }
 
-    std::vector<std::string> Query_data::getPrimaryKeys() {
+    std::vector<std::string> &Query_data::getPrimaryKeys() {
         return _primaryKey;
     }
 
-    std::vector<Query_condition> Query_data::getConditions() const {
+    std::vector<Query_condition> &Query_data::getConditions()  {
         return _conditions;
     }
 
-    std::vector<Value*> Query_data::getColumnValues() {
+    std::vector<Value*> &Query_data::getColumnValues() {
         return _values;
 
     }
 
-    const std::vector<std::string> &Query_data::getColumnName() const {
-        return _columnName;
+     std::vector<std::string> &Query_data::getColumnNames() {
+        return _columnNames;
+    }
+
+    void Query_data::set_queryType(Query_type _queryType) {
+        Query_data::_queryType = _queryType;
     }
 }
